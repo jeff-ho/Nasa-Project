0eb80736209ea084de9bbf41236d597821f1c818
Jeff <jeff.ho@hotmail.com>
2023-03-16 14:49:13 -0400

    Connected app to SpaceX API

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,19 @@
 // Code in here is not express code. Only server code. Express code is in app.js file. 
-
 const http = require('http')
+
+require('dotenv').config()
+
 const  app  = require('./app.js')
 const { mongoConnect } = require('./services/mongo')
 const {loadPlanetsData} = require('./models/planets.model')
+const {loadLaunchData} = require('./models/launches.model')
+
 
 // app express object acts as a listener for our built in Node http module to any req coming into the server. 
 // Any middleware or routehandling applied to app object will be able to go to our 
 // server
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 const server = http.createServer(app)
@@ -18,6 +22,7 @@ const server = http.createServer(app)
 async function startServer() {
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchData();
 
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGO_URL = 
+const MONGO_URL = process.env.MONGO_URL
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!')
@@ -8,7 +8,6 @@ mongoose.connection.once('open', () => {
 mongoose.connection.on('error', (err) => {
     console.error(err)
 })
-
 
 async function mongoConnect() {
    await mongoose.connect(MONGO_URL)
